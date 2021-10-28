a50235d88a44382cb04a3a449e628594f973b4dd
Laylo Khodjaeva <khodjaeva.laylo@gmail.com>
2021-10-28 17:19:40 +0500

    Add new queries

--- queries.sql
+++ queries.sql
@@ -1,5 +1,4 @@
-SELECT *
-FROM animals
+SELECT * FROM animals
 WHERE name LIKE '%mon';
 
 SELECT name
@@ -28,4 +27,42 @@ WHERE name <> 'Gabumon';
 
 SELECT *
 FROM animals
-WHERE weight_kg BETWEEN 10.4  AND 17.3
+WHERE weight_kg BETWEEN 10.4  AND 17.3;
+
+
+BEGIN;
+UPDATE animals
+SET species='unspecified';
+ROLLBACK;
+
+BEGIN;
+UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon';
+UPDATE animals SET species = 'pokemon' WHERE species IS NULL;
+COMMIT;
+
+BEGIN;
+DELETE FROM animals;
+ROLLBACK;
+
+BEGIN;
+DELETE FROM animals
+WHERE date_of_birth > '2022-01-01';
+SAVEPOINT delete_animal;
+UPDATE animals SET weight_kg = weight_kg * -1;
+SAVEPOINT delete_animal;
+UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg > 0;
+COMMIT;
+
+SELECT COUNT(*) FROM animals;
+
+SELECT COUNT(*) FROM animals WHERE escape_attempts = 0;
+
+SELECT AVG(weight_kg) FROM animals;
+
+SELECT neutered, MAX(escape_attempts) FROM animals GROUP BY neutered;
+
+SELECT species, MIN(weight_kg), MAX(weight_kg) FROM animals GROUP BY species;
+
+SELECT species, AVG(escape_attempts) FROM animals 
+WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31'
+GROUP BY species;
