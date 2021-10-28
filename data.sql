3df1c2a89599b7e0742f4e53dd3cd005f2ae1990
Laylo Khodjaeva <khodjaeva.laylo@gmail.com>
2021-10-29 00:39:24 +0500

    Add new data

--- data.sql
+++ data.sql
@@ -1,5 +1,4 @@
-INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg, species) VALUES(
-    1,
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES(
 	'Agumon',
 	'2020-02-03',
 	0,
@@ -8,8 +7,7 @@ INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_
 	
 );
 
-INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg, species) VALUES(
-    2,
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES(
 	'Gabumon',
 	'2018-11-15',
 	2,
@@ -18,8 +16,7 @@ INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_
 	
 );
 
-INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg, species) VALUES(
-    3,
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES(
 	'Pikachu',
 	'2021-01-07',
 	1,
@@ -28,8 +25,7 @@ INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_
 	
 );
 
-INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg, species) VALUES(
-	4,
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES(
 	'Devimon',
 	'2017-05-12',
 	5,
@@ -38,8 +34,7 @@ INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_
 	
 );
 
-INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg, species) VALUES(
-    5,
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES(
 	'Charmander',
 	'2020-02-08',
 	0,
@@ -48,8 +43,7 @@ INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_
 	
 );
 
-INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg, species) VALUES(
-    6,
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES(
 	'Plantmon',
 	'2022-11-15',
 	3,
@@ -58,8 +52,7 @@ INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_
 	
 );
 
-INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg, species) VALUES(
-    7,
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES(
 	'Squirtle',
 	'1993-04-02',
 	1,
@@ -68,8 +61,7 @@ INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_
 	
 );
 
-INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg, species) VALUES(
-	8,
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES(
 	'Angemon',
 	'2005-06-12',
 	1,
@@ -78,8 +70,7 @@ INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_
 	
 );
 
-INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg, species) VALUES(
-    9,
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES(
 	'Boarmon',
 	'2005-06-07',
 	7,
@@ -88,8 +79,7 @@ INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_
 	
 );
 
-INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg, species) VALUES(
-	10,
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES(
 	'Blossom',
 	'1998-10-13',
 	3,
@@ -97,3 +87,56 @@ INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_
 	17
 	
 );
+
+-- OWNERS INFORMATION
+
+INSERT INTO owners(owners_id, name, age) VALUES(
+	1, 
+	'Sam Smith', 
+	34
+);
+
+INSERT INTO owners(owners_id, name, age) VALUES(
+	2, 
+	'Jennifer Orwell', 
+	19
+);
+
+INSERT INTO owners(owners_id, name, age) VALUES(
+	3, 
+	'Bob', 
+	45
+);
+
+INSERT INTO owners(owners_id, name, age) VALUES(
+	4, 
+	'Melody Pond', 
+	77
+);
+
+INSERT INTO owners(owners_id, name, age) VALUES(
+	5, 
+	'Dean Winchester', 
+	14
+);
+
+INSERT INTO owners(owners_id, name, age) VALUES(
+	6, 
+	'Jodie Whittaker', 
+	38
+);
+
+-- SPECIES INFORMATION
+
+INSERT INTO species(species_id, name) VALUES(
+	1,
+	'Pokemon'
+);
+
+INSERT INTO species(species_id, name) VALUES(
+	2,
+	'Digimon'
+);
+
+
+
