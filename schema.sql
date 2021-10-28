aad0af2685625739c32583751aee7edafcee79c6
Laylo Khodjaeva <khodjaeva.laylo@gmail.com>
2021-10-29 00:23:25 +0500

    Create new tables

--- schema.sql
+++ schema.sql
@@ -1,11 +1,27 @@
 CREATE TABLE animals(
- id int PRIMARY KEY NOT NULL,
+ animals_id int NOT NULL GENERATED ALWAYS AS IDENTITY,
+ owners_id int,
+ species_id int,
  name VARCHAR(30),
  date_of_birth date NOT NULL,
  escape_attempts integer,
  neutered boolean NOT NULL,
- weight_kg decimal NOT NULL
+ weight_kg decimal NOT NULL,
+ PRIMARY KEY(animals_id),
+ FOREIGN KEY(owners_id) REFERENCES owners(owners_id),
+ FOREIGN KEY(species_id) REFERENCES species(species_id)
+	
 );
 
-ALTER TABLE animals
-ADD  species VARCHAR(50)
+CREATE TABLE owners(
+owners_id int NOT NULL,
+name VARCHAR(30),
+age integer,
+PRIMARY KEY(owners_id)
+);
+
+CREATE TABLE species(
+species_id int NOT NULL,
+name VARCHAR(30),
+PRIMARY KEY(species_id)
+);
