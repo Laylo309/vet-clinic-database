a5ac19e5a9d9c9ae4500fd7c03000efe4449aff4
Laylo Khodjaeva <khodjaeva.laylo@gmail.com>
2021-10-28 16:29:47 +0500

    Add new data

--- data.sql
+++ data.sql
@@ -37,3 +37,63 @@ INSERT INTO animals VALUES(
 	11
 	
 );
+
+INSERT INTO animals VALUES(
+    5,
+	'Charmander',
+	'2020-02-08',
+	0,
+	false,
+	-11
+	
+);
+
+INSERT INTO animals VALUES(
+    6,
+	'Plantmon',
+	'2022-11-15',
+	3,
+	true,
+	-5.7
+	
+);
+
+INSERT INTO animals VALUES(
+    7,
+	'Squirtle',
+	'1993-04-02',
+	1,
+	false,
+	-12.13
+	
+);
+
+INSERT INTO animals VALUES(
+	8,
+	'Angemon',
+	'2005-06-12',
+	1,
+	true,
+	-45
+	
+);
+
+INSERT INTO animals VALUES(
+    9,
+	'Boarmon',
+	'2005-06-07',
+	7,
+	true,
+	20.4
+	
+);
+
+INSERT INTO animals VALUES(
+	10,
+	'Blossom',
+	'1998-10-13',
+	3,
+	true,
+	17
+	
+);
--- schema.sql
+++ schema.sql
@@ -1,9 +1,9 @@
 CREATE TABLE animals(
  id int PRIMARY KEY NOT NULL,
- name VARCHAR(100),
+ name VARCHAR(30),
  date_of_birth date NOT NULL,
  escape_attempts integer,
  neutered boolean NOT NULL,
  weight_kg decimal NOT NULL,
- species VARCHAR(50),
+ species VARCHAR(50)
 );
