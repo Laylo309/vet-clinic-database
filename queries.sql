ada2bc7c2793dc857f626b1db0e132a0951e1c11
Laylo Khodjaeva <khodjaeva.laylo@gmail.com>
2021-10-28 20:28:24 +0500

    Make required chages

--- queries.sql
+++ queries.sql
@@ -35,11 +35,14 @@ UPDATE animals
 SET species='unspecified';
 SELECT species FROM animals;
 ROLLBACK;
+SELECT species from animals; 
 
 BEGIN;
 UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon';
 UPDATE animals SET species = 'pokemon' WHERE species IS NULL;
+SELECT species from animals;
 COMMIT;
+SELECT species from animals;
 
 BEGIN;
 DELETE FROM animals;
@@ -50,7 +53,8 @@ DELETE FROM animals
 WHERE date_of_birth > '2022-01-01';
 SAVEPOINT delete_animal;
 UPDATE animals SET weight_kg = weight_kg * -1;
-SAVEPOINT delete_animal;
+ROLLBACK TO SAVEPOINT delete_animal;
+
 UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg > 0;
 COMMIT;
 
