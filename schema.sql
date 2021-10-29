f3389b020403c5b7ca2162fa92e08dc31980d7fb
Laylo Khodjaeva <khodjaeva.laylo@gmail.com>
2021-10-29 22:52:45 +0500

    Make rerquired changes

--- schema.sql
+++ schema.sql
@@ -1,16 +1,12 @@
 CREATE TABLE animals(
  animals_id int NOT NULL GENERATED ALWAYS AS IDENTITY,
- owners_id int,
- species_id int,
  name VARCHAR(30),
  date_of_birth date NOT NULL,
  escape_attempts integer,
  neutered boolean NOT NULL,
  weight_kg decimal NOT NULL,
- PRIMARY KEY(animals_id),
- FOREIGN KEY(owners_id) REFERENCES owners(owners_id),
- FOREIGN KEY(species_id) REFERENCES species(species_id)
-	
+ species VARCHAR(50),
+ PRIMARY KEY(animals_id)
 );
 
 CREATE TABLE owners(
@@ -25,3 +21,6 @@ species_id int NOT NULL,
 name VARCHAR(30),
 PRIMARY KEY(species_id)
 );
+
+ALTER TABLE animals ADD owners_id int REFERENCES owners(owners_id);
+ALTER TABLE animals ADD species_id int REFERENCES species(species_id);
